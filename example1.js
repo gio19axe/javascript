a9477a27627f21c6be27c82fc2454d82467b33b3
gio19axe <jovanialmonte@gmail.com>
2020-09-23 16:37:02 -0500

    ejemplos

---
+++ example1.js
@@ -0,0 +1,14 @@
+function Example() {
+
+   // Case sentitive
+   var myData = 'Hello';
+   
+  // Bad reference
+   //mydata
+
+   myData = myData + ' World';
+
+  // document.write( myData );
+   console.log( myData );
+}
+Example();
